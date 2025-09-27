607e5f5afec8705424bdcbac436aeb753c829dca
Amos Thacker-Gwaltney <amostg3613@gmail.com>
2025-09-27 16:56:06 -0400

    removed useless static modifiers

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 #define TOWN_MPG 23.5
 #define GAS_TANK_SIZE 20
 
-static const float TOWN_MAX_MILES = (GAS_TANK_SIZE * TOWN_MPG);
-static const float HIGHWAY_MAX_MILES = (GAS_TANK_SIZE * HIGHWAY_MPG);
+const float TOWN_MAX_MILES = (GAS_TANK_SIZE * TOWN_MPG);
+const float HIGHWAY_MAX_MILES = (GAS_TANK_SIZE * HIGHWAY_MPG);
 
-static const char *TOWN_MILES_MSG = "Max distance in miles traveled in town per tank of gas = ";
-static const char *HIGHWAY_MILES_MSG = "Max distance in miles traveled on the highway per tank of gas = "; 
-static const char *INIT_MSG = "## Max Distances in miles traveled per tank of gas in different settings ##";
+const char *TOWN_MILES_MSG = "Max distance in miles traveled in town per tank of gas = ";
+const char *HIGHWAY_MILES_MSG = "Max distance in miles traveled on the highway per tank of gas = "; 
+const char *INIT_MSG = "## Max Distances in miles traveled per tank of gas in different settings ##";
 
 int main() {
 	printf("\n%s\n\n\n", INIT_MSG);
