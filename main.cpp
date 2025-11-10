4887c73f994cd0e420f144f76823b61c3e5cc007
Amos Thacker-Gwaltney <morphouswatery@gmail.com>
2025-11-10 15:52:17 -0500

    Update main.cpp
    Add some comments to explain the code

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@ auto const chart_header_msg =
 
 
 int main(void) {
+  // Opens the user inputted data path and checks if it opened correctly
   filesystem::path data_path = query(path_query); 
   ifstream data_ifstream(data_path);
   if (!data_ifstream.is_open()) {
@@ -25,20 +26,26 @@ int main(void) {
     exit(1);
   }
 
+  // Queries the user for the town name
   string town_name = query(town_name_query);
 
   cout << '\n' << town_name << ' ' 
     << chart_header_msg << endl;
 
+  // Iterates over each line of the data file, for each line,
+  // the year increases by 20 as per the directions.
   int pop;
   for (int year = 1900; data_ifstream >> pop; year += 20) {
     string asts;
     int ast_num = pop / 1000;
-
+    
+    // adds an asterisk for each 1,000 in the current line's integer
+    // which is supposed to represent population in the user's inputted town name
     for (int i = 0; i < ast_num; ++i) {
       asts = asts + '*';
     }
 
+    // prints the bar chart line, as per the directions.
     cout << year << ' ' << asts << endl; 
   }
 
