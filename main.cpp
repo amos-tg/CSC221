ca7c14fe6293217f66d29367936330e96a847e8f
Amos Thacker-Gwaltney <amostg3613@gmail.com>
2025-11-10 15:23:14 -0500

    Seems to be working correctly.

--- main.cpp
+++ main.cpp
@@ -1,76 +1,47 @@
-#include <iostream>
 #include <fstream>
+#include <string>
 #include <filesystem>
+#include "query.cpp"
 
 
 using namespace std;
 
 
-ifstream get_roster_ifstream(void);
+auto const path_query = 
+  "What is the name of the data file? : ";
 
+auto const town_name_query = 
+  "What is the name of the Town? : ";
 
-const auto roster_fname = "LineUp.txt";
-const auto fexists_err = "Error: file does not exist";
-const auto fopen_err = "Error: failed to open file";
+auto const chart_header_msg = 
+  "Population Growth\n(each * represents 1,000 people)";
 
 
 int main(void) {
-  ifstream roster_ifstream = get_roster_ifstream();
-
-  // pull out the first name so I don't compare 
-  // against uninitialized strings in the loop.
-  string front, back, current;
-  roster_ifstream >> current;
-  front = back = current;
-  current.clear();
-
-  // counter must be initialized outside of the loop scope
-  // counter starts from one to account for the initialization
-  int i;
-  for (i = 1; roster_ifstream >> current; ++i) { 
-    if (current < front) {
-      front = current; 
-    } 
-
-    if (current > back) {
-      back = current;
-    }
-
-    /* this is how I tested the program. 
-    cout << "current, i: " << i << " : " << current << endl;
-    cout << "front, i: " << i << " : " << front << endl;
-    cout << "back, i: " << i << " : " << back << '\n' << endl;*/
-
-    current.clear();
-  }
-
-  cout << '\n' << "Front of the line: " << front << endl;
-  cout << "Back of the line: " << back << endl;
-  cout << "Total number of students: " << i << endl; 
-}
-
-/// returns the roster_ifstream for the LineUp.txt file
-/// which must be present in the current working directory
-/// as dictated by filesystem::current_path.
-ifstream get_roster_ifstream(void) { 
-  ifstream roster_ifstream;
-
-  // gets LineUp.txt from cwd
-  filesystem::path name_list_path = 
-    filesystem::current_path().append(roster_fname);
-
-  // checks to make sure the file exists
-  if (!filesystem::exists(name_list_path)) {
-    cerr << fexists_err << endl;
-    exit(1); 
-  }
-
-  // opens the file and checks if it opened without err.
-  roster_ifstream.open(name_list_path);
-  if (!roster_ifstream.is_open()) {
-    cerr << fopen_err << endl; 
+  filesystem::path data_path = query(path_query); 
+  ifstream data_ifstream(data_path);
+  if (!data_ifstream.is_open()) {
+    cerr << "Error: ifstream open method failed" << endl;
     exit(1);
   }
-  
-  return roster_ifstream;
-}
+
+  string town_name = query(town_name_query);
+
+  cout << '\n' << town_name << ' ' 
+    << chart_header_msg << endl;
+
+  int pop;
+  for (int year = 1900; data_ifstream >> pop; year += 20) {
+    string asts;
+    int ast_num = pop / 1000;
+
+    for (int i = 0; i < ast_num; ++i) {
+      asts = asts + '*';
+    }
+
+    cout << year << ' ' << asts << endl; 
+  }
+
+  return 0;
+} 
+
