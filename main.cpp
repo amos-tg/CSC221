70173946c7c31887913a6991a7ece8f10b5c100d
Amos Thacker-Gwaltney <amostg3613@gmail.com>
2025-11-12 23:30:32 -0500

    working as well as the directions want it.

--- main.cpp
+++ main.cpp
@@ -1,86 +1,94 @@
+#define NUM_REGIONS 5
 #include <iostream>
-
-
+#include <string>
 using namespace std;
 
-
-void getLengthWidth(double& length, double& width); 
-double calcPerimeter(double length, double width);
-double calcArea(double length, double width);
-void displayProperties(double perimeter, double area);
-
+void getRegionInfo(string& region, int& num_accidents);
+bool isLower(int x, int y);
+void showLowest(string region, int num_accidents);
 
 int main(void) {
-  const auto repeat_msg = 
-    "Do you want to repeat the process? : (y/n)";
-  const auto repeat_input_err = 
-    "Error: invalid choice, use y, Y, n, or N.";
+  string region = ""; 
+  // comparing against uninitialized int is undefined so
+  // I'm marking this for the getRegionInfo function.
+  int num_accidents = -1;
 
-  double length, width; 
-  char repeat; 
+  for (int i = 0; i < NUM_REGIONS; ++i) {
+    getRegionInfo(region, num_accidents);
+  }
 
-  do { 
-    cout << '\n';
-
-    getLengthWidth(length, width);
-    /// cpp passes by value like c, so no need to worry 
-    /// about moving the length and width before calcArea
-    double perimeter = calcPerimeter(length, width);
-    double area = calcArea(length, width);
-    displayProperties(perimeter, area);
-
-    cout << repeat_msg << endl;
-    cin >> repeat;
-    switch (repeat) { 
-      case 'y':
-        break;
-      case 'Y':
-        break;
-      case 'n': 
-        repeat = 0;
-        break;
-      case 'N':
-        repeat = 0;
-        break;
-      default: 
-        cerr << repeat_input_err << endl;
-        exit(1);
-    }
-  } while (repeat);
+  showLowest(region, num_accidents);
 
   return 0;
 }
 
-void getLengthWidth(double& length, double& width) {
-  const auto length_query = "What is the length of the rectangle? ";
-  const auto width_query = "What is the width of the rectangle? ";
-  const auto double_cin_err = 
-    "Error: user input is invalid, x > 0 && no non-integrals";
+void getRegionInfo(string& region, int& num_accidents) {
+  string region_cmp;
+  unsigned int num_accidents_cmp;
 
-  cout << length_query;
-  cin >> length;  
-  if (cin.fail() || !(length > 0)) {
-    cerr << double_cin_err << endl;
-    exit(1);
+  const auto region_query = "What is the name of the region? : ";
+  const auto num_accid_query = "What is the number of accidents in the region? : ";
+
+  // handles initialization / first iteration
+  // skips over remaining code, goes to the
+  // input validation for num_accidents
+  if (num_accidents == -1) {
+    // gets region
+    cout << region_query;
+    cout.flush();
+    cin >> region; 
+
+    // gets number accidents
+    cout << num_accid_query;
+    cout.flush();
+    cin >> num_accidents;
+
+    // spaces things out
+    cout << endl;
+
+    goto validate_input;
   }
 
-  cout << width_query;
-  cin >> width;
-  if (cin.fail() || !(length > 0)) {
-    cerr << double_cin_err << endl;
-    exit(1);
+  // gets region
+  cout << region_query;
+  cout.flush();
+  cin >> region_cmp;
+
+  // gets number accidents
+  cout << num_accid_query;
+  cout.flush();
+  cin >> num_accidents_cmp;
+
+  // spaces things out
+  cout << endl;
+
+  if (isLower(num_accidents_cmp, num_accidents)) {
+    num_accidents = num_accidents_cmp;
+    region = region_cmp;
   }
+
+  validate_input:
+    if (num_accidents < 0) {
+      cerr << "Error: (number of accidents) < 0" << endl;
+      exit(1);
+    } 
+    
+  return;
 }
 
-double calcPerimeter(double length, double width) {
-  return 2 * (length + width);
-}
- 
-double calcArea(double length, double width) {
-  return length * width;
+// The directions have the function named
+// isLower but the definition says <=, I may have 
+// typed it by accident or it might be a typo.
+// Also this doesn't account for regions with equal
+// numbers of accidents. 
+// returns true if x is less than y.
+bool isLower(int x, int y) {
+  return (x <= y);
 }
 
-void displayProperties(double perimeter, double area) {
-  cout << "The perimeter = " << perimeter << '\n'
-    << "The area = " << area << endl;
+void showLowest(string region, int num_accidents) {
+  cout << region << " had the lowest number of accidents : " 
+    << num_accidents << endl;
+
+  return;  
 }
