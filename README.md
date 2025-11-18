148644e6b8da62998454308d3c8bb3162f0c200f
Amos Thacker-Gwaltney <amostg3613@gmail.com>
2025-11-18 13:19:28 -0500

    Seems to be working

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-# Reminder for Professor Sprague: 
+# CPP Version Notice:
+I use <format> and std::format in this program which is from c++23, I don't know if you have to do anything extra to compile this on windows because I'm on linux, but I looked it up and the internet says that c++23 is supported on windows with Visual Studio.
 
+# Linux Compilation:
+g++ -std=c++23 main.cpp    
+
+# Reminder for Professor Sprague: 
 the github accounts amos-tg & boodledot1 are both me. 
 
 I have a laptop and a desktop, and they have different github accounts.
--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #define CELSIUS_ARR_LEN 20
 #define TABLE_LENGTH 38
 #define TABLE_MIDDLE 19
-// table length and table middle don't include the \n\t prefix
 
 #include <string>
 #include <iostream>
-#include <iomanip>
+#include <format>
 
 using namespace std;
 
@@ -16,11 +15,15 @@ int main(void) {
   // least to greatest celsius vals from 0 to 20.
   double celsius_ltg[CELSIUS_ARR_LEN];
 
+  // loops over the range of requested fahrenheit values and assigns
+  // the celsius equivalent in the array at the index fahrenheit value i.e. 
+  // celsius_ltg[32] would equal 0 because (32F = 0C)
   for (int fahr = 0; fahr <= 20; ++fahr) {
     // assigns to array from 0 too 20 
     celsius_ltg[fahr] = getCelsius(fahr); 
   }
 
+  // messy messy messy
   printTable(celsius_ltg);
 
   return 0;
@@ -35,13 +38,19 @@ double getCelsius(double fahrenheit) {
    return (fahrenheit - 32) * fiveOver9;
 }
 
+/// takes the celsius table which maps to  
+/// fahrenheit values by index and formats 
+/// the output based on the table before printing
+/// the right adjusted graph.
 void printTable(double celsius_table[]) {
   int i;
 
   string table = "\
-\n\t_____________________________________\
-\n\t| Fahrenheit and Celsius Equivalent |\
-\n\t|-----------------------------------|";
+\n\t|------------------------------------|\
+\n\t| Fahrenheit and Celsius Equivalents |\
+\n\t|------------------------------------|\
+\n\t|       Fahrenheit |         Celsius |\
+\n\t|------------------------------------|";
 
   // generates the seperator which is inserted below each content line.
   string seperator = "\n\t|";
@@ -52,7 +61,7 @@ void printTable(double celsius_table[]) {
 
   // generates the formatted content line and adds it to the 
   // table appending the seperator afterward.
-  for (i = 0; i < CELSIUS_ARR_LEN; ++i) {
+  for (i = 0; i <= CELSIUS_ARR_LEN; ++i) {
     int i2, offset;
     string table_item = "|";
 
@@ -63,16 +72,17 @@ void printTable(double celsius_table[]) {
     table_item += '|';
     table_item[TABLE_MIDDLE] = '|';
 
+    // this sets the fahrenheit value inside the content line right adjusted
     string fahrenheit = to_string(i);
     int fahr_len = fahrenheit.length();
-    for (offset = (fahr_len - 1), i2 = 0; i2 < fahr_len; ++i2, --offset) {
+    for (offset = (fahr_len + 1), i2 = 0; i2 < fahr_len; ++i2, --offset) {
       table_item[TABLE_MIDDLE - offset] = fahrenheit[i2];
     }
 
-    string celsius{ "" }; 
-
+    // This sets the celsius value inside the content line right adjusted
+    string celsius = format("{:.1F}", celsius_table[i]);
     int celsius_len = celsius.length();
-    for (offset = (celsius_len - 1), i2 = 0; i2 < celsius_len; ++i2, --offset) {
+    for (offset = (celsius_len + 2), i2 = 0; i2 < celsius_len; ++i2, --offset) {
       table_item[TABLE_LENGTH - offset] = celsius[i2];
     }
 
