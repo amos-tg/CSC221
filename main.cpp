f4761a3763b9d6f4a1005f404a8d24cc584bfab6
Amos Thacker-Gwaltney <morphouswatery@gmail.com>
2025-12-01 19:39:46 -0500

    comments on function definition signatures

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #define NUM_REGIONS 5
 #include <iostream>
 #include <string>
+
+
 using namespace std;
+
 
 void getRegionInfo(string& region, int& num_accidents);
 bool isLower(int x, int y);
 void showLowest(string region, int num_accidents);
+
 
 int main(void) {
   string region = ""; 
@@ -22,6 +26,12 @@ int main(void) {
   return 0;
 }
 
+/// takes a reference to the string:region, and the int:num_accidents
+/// takes user input to gather the region name and number of accidents
+/// in said region, if the region has the lowest number of accidents so
+/// far, it replaces the old region name with the new one as well as the 
+/// accident count. If the number of accidents inputed is less than zero
+/// the program will return an error.
 void getRegionInfo(string& region, int& num_accidents) {
   string region_cmp;
   unsigned int num_accidents_cmp;
@@ -67,6 +77,7 @@ void getRegionInfo(string& region, int& num_accidents) {
     region = region_cmp;
   }
 
+  // the infamous goto label
   validate_input:
     if (num_accidents < 0) {
       cerr << "Error: (number of accidents) < 0" << endl;
@@ -81,11 +92,13 @@ void getRegionInfo(string& region, int& num_accidents) {
 // typed it by accident or it might be a typo.
 // Also this doesn't account for regions with equal
 // numbers of accidents. 
-// returns true if x is less than y.
+/// returns true if x is less than y.
 bool isLower(int x, int y) {
   return (x <= y);
 }
 
+/// takes the region with the lowest accidents and it's 
+/// accompanying number of accidents and prints it out.
 void showLowest(string region, int num_accidents) {
   cout << region << " had the lowest number of accidents : " 
     << num_accidents << endl;
