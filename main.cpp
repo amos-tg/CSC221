83bc9ffafa4369487cce29609f1624af1739fae8
Amos Thacker-Gwaltney <amostg3613@gmail.com>
2025-12-12 17:24:42 -0500

    Partially working, there is at least one bug

--- main.cpp
+++ main.cpp
@@ -1,105 +1,275 @@
+#include <exception>
 #include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
-
 using namespace std;
 
-// the fact that the world series was not played in 1904 and 1994 has no 
-// effect on the program, because the date is not displayed and since no one
-// played the world series in those years there will be no additional data to filter.
+/// clears the screen using ANSI escape codes.
+void clearScreen(void);
 
-/// file_path: the name of the file containing the team names.
-/// team_names: reference to a vector of string team names.
-///
-/// Reads the team names from file_path into the team_names 
-/// vector. Returns an error if the file open or reading fails.
-void getTeams(const string file_path, vector<string> &team_names);
+/// clears a single line using ANSI escape codes.
+void clearLine(void);
 
-/// team_name: the name of the team to find
-/// team_names: the vector of winners from 1903 to 2012 excluding 1904 and 1994
+/// board: 3x3 board array.
 ///
-/// returns the number of times that team_name occurs in the team_names vector of winners. 
-unsigned findWinners(const string query_team_name, const vector<string> &team_names);
+/// Takes an empty 3x3 array, initializes the board to '*' in all positions.
+void initializeBoard(char (&board)[3][3]);
+
+/// board: 3x3 board array.
+///
+/// Takes a 3x3 board array, the row and column numbers and cell contents of 
+/// the array are displayed.
+void displayBoard(char (&board)[3][3]);
+
+/// token: the token to be placed.
+///
+/// board: the 3x3 board array.
+///
+/// uses getLocation(row, col) to get the position to  
+/// place the token at.
+void placeToken(char token, char (&board)[3][3]);
+
+/// row: takes the row at which to place the token
+///
+/// col: takes the column at which to place the token
+///
+/// gets and validates the row and column at which to place a token
+void getLocation(unsigned short &row, unsigned short &column, char token);
+
+/// The options returned by getBoardState which indicate the game state.
+enum { PLAY, TIE, X_WIN, O_WIN };
+
+/// token: token checked 
+/// 
+/// board: 3x3 array of the board state
+/// 
+/// ouputs PLAY, TIE, X_WIN, or O_WIN based on the board state
+unsigned getBoardState(char token, char (&board)[3][3]);
+
+/// The options returned by checkBoard which indicate the board state
+enum { SPACE_LEFT, NO_SPACE, WINNER };
+
+/// token: the token to check for winning 3 in a row state
+///
+/// board: the 3x3 array checked for the various states
+///
+/// Checks for a row winner, a column winner, or a diagonal winner
+/// if no winner is found, checks to see if there are still available 
+/// cells in the board array. Returns SPACE_LEFT, NO_SPACE, WINNER.
+unsigned checkBoard(char token, char (&board)[3][3]);
 
 int main(void) {
-  char team_names_path[] = "./Teams.txt";
-  char winner_team_names_path[] = "./WorldSeriesWinners.txt";
-  vector<string> team_names{}, winner_team_names{};
-  string query_team_name;
+  char board[3][3];
 
-  // read the team names and the winner team names into vectors
-  getTeams(team_names_path, team_names); 
-  getTeams(winner_team_names_path, winner_team_names);
+  clearScreen();
+  initializeBoard(board);
+  displayBoard(board);
+  
+  char token = 'X';
+  for (int i = 1; i < 10; ++i) {
+    placeToken(token, board);
 
-  cout << "Team Names:" << '\n';
-  for (string &team_name: team_names) {
-    cout << team_name << '\n';
-  }
-
-  cout.flush();
-
-  // loop over the team name query and display information
-  // until user enters quit.
-  for (;;) {
-    cout << '\n' << "Enter the name of a team (enter quit to end): ";
-    cout.flush();
-    if (!getline(cin, query_team_name)) {
-      cerr << "Error: failed to read invalid team name input" << endl;
-      terminate();
+    if (token == 'X') {
+      token = 'O';   
+    } else {
+      token = 'X';
     }
 
-    if (query_team_name == "quit") {
-      break;
+    clearScreen();
+
+    auto win_msg = " Won the Game!";
+    switch (getBoardState(token, board)) {
+      case X_WIN:
+        i = 10;
+        cout << 'X' << win_msg << endl;
+        break;
+      case O_WIN:
+        i = 10;
+        cout << 'O' << win_msg << endl;
+        break;
+      case TIE:
+        i = 10;
+        cout << "Nobody" << win_msg << endl;
+        break;
+      case PLAY:
+        break;
     }
 
-    // if the team isn't in the list of teams tell the user and reset
-    if (!findWinners(query_team_name, team_names)) {
-      cout << "Team name wasn't on the list, try again..." << endl;
-      continue;
-    }
+    displayBoard(board);
+  } 
 
-    int num_wins = findWinners(query_team_name, winner_team_names);
-
-    cout << '\n' << query_team_name << " won the World Series " 
-      << num_wins << " times." << '\n' << endl;
-  }
 
   return 0;
 }
 
-void getTeams(string file_path, vector<string> &team_names) {
-  string tname;
+void clearScreen(void) {
+  cout << "\033[2J\033[H"; 
+  cout.flush();
+}
 
-  // opens the file and checks if it opened
-  ifstream file(file_path);
-  if (!file) {
-    cerr << "Error: failed to open file" << file_path << endl;
-    terminate();
-  }
+void clearLine(void) {
+  cout << "\033[2K";
+  cout.flush();
+}
 
-  while (getline(file, tname)) {
-    // having an issue where '\r' postfixes every team name.
-    // I think windows.txt files have a carriage line return or
-    // something wierd going on that doesn't work on linux.
-    if (tname[tname.length() - 1] == '\r') {
-      tname.pop_back();
+void initializeBoard(char (&board)[3][3]) {
+  for (char (&row)[3]: board) {
+    for (char &square: row) {
+      square = '*';
     }
-    
-    // pushes the team name onto team names vector line by line
-    team_names.push_back(tname);
   }
 }
 
-unsigned findWinners(string query_team_name, const vector<string> &team_names) {
-  // iterates over the team_names and increments a counter for every match
-  // of the query_team_name.
-  unsigned counter{};
-  for (const string &tname: team_names) {
-    if (tname == query_team_name) { 
-      ++counter; 
+void displayBoard(char (&board)[3][3]) {
+  cout << '\n' << "   | 1 | 2 | 3 |" 
+    << '\n' << "----------------" << '\n';
+
+  for (int row = 0; row < 3; ++row) {
+    cout << ' ' << (row+1) << " | ";
+
+    for (char &token: board[row]) {
+      cout << token << " | "; 
+    }
+
+    cout << '\n' << "----------------" << '\n';
+  }  
+
+  cout.flush();
+}
+
+void placeToken(char token, char (&board)[3][3]) {
+  unsigned short row, column;  
+  auto taken_msg = 
+    "The location inputted is already taken... (hit enter to try again)";
+
+  for (;;) {
+    string enter; 
+
+    getLocation(row, column, token);
+
+    switch (board[row][column]) {
+      case 'X':
+        cout << taken_msg << endl;
+        cin >> enter;
+        continue;
+        break;
+      case 'O':
+        cout << taken_msg << endl;
+        cin >> enter;
+        continue;
+        break;
+    }
+
+    board[row][column] = token;
+    break;
+  }
+}
+
+void getLocation(unsigned short &row, unsigned short &column, char token) {
+  auto input_err = "Error: invalid input";
+
+  cout << "Input the row to place the " << token << " at: ";
+  cin >> row;
+  if (cin.fail() || row > 3 || row < 1) {
+    cerr << input_err << endl; 
+    exit(1);
+  }
+
+  clearLine();
+  cout.flush();
+
+  cout << "Input the column to place the " << token << " at: ";
+  cin >> column;
+  if (cin.fail() || column > 3 || column < 1) {
+    cerr << input_err << endl; 
+    exit(1);
+  }
+
+  clearLine();
+  cout.flush();
+
+  // account for zero based indexing
+  --row;
+  --column;
+}
+
+unsigned getBoardState(char token, char (&board)[3][3]) {
+  switch (checkBoard(token, board)) {
+    case SPACE_LEFT:
+      return PLAY; 
+      break;
+    case WINNER:
+      if (token == 'X') {
+        return X_WIN;
+      } else if (token == 'O') {
+        return O_WIN;
+      }
+
+      break;
+    case NO_SPACE:
+      return TIE;
+      break;
+  } 
+
+  cerr << "Error: unreachable control flow" << endl;
+  exit(1);
+}
+
+unsigned checkBoard(char token, char (&board)[3][3]) {
+  // check rows
+  for (char (&row)[3]: board) {
+    for (int i = 0; i < 3; ++i) {
+      if (token != row[i]) {
+        continue;
+      } else if (i == 2) {
+        return WINNER;
+      }
+    }
+  }
+  
+  // check columns
+  for (int col = 0; col < 3; ++col) {
+    for (int row = 0; row < 3; ++row) {
+      if (token != board[row][col]) {
+        continue;
+      } else if (row == 2) {
+        return WINNER;
+      }
+    }
+  }                            
+  
+  // handles top left to bottom right diagonal 
+  //
+  // Index Representation:
+  // (row:0, col:0) -> (row:1, col:1) -> (row:2, col:2)
+  for (int row{}, col{}; row < 3; ++col, ++row) {
+    if (token != board[row][col]) {
+      break;
+    } else if (row == 2) {
+      return WINNER;
+    }
+  } 
+
+  // handles the bottom left to top right diagonal 
+  //
+  // Index Representation:
+  // (row:2, col:0) -> (row 1, col:1) -> (row:0, col:2)
+  for (int row = 2, col = 0; col < 3; ++col, --row) {
+    if (token != board[row][col]) {
+      break;
+    } else if (col == 2) {
+      return WINNER;
     }
   }
 
-  return counter;
+  // nothing matched, so we check for space now
+  for (char (&row)[3]: board) {
+    for (char &spot: row) {
+      if ('*' == spot) {
+        return SPACE_LEFT;
+      }
+    }
+  }
+
+  // we didn't find any winners or space so we return NO_SPACE
+  return NO_SPACE;
 }
+
